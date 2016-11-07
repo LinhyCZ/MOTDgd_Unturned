f2847ad1b233016122ea505274924abb30a752cd
LinhyCZ <tomaslinhys@gmail.com>
2016-11-07 17:03:55 +0100

    Initial upload

---
+++ MOTDgd/Commands.cs
@@ -0,0 +1,302 @@
+﻿using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace MOTDgd
+{
+    class CommandAd : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Player; }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "ad";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Generates link to advertisement page. After completion gives player reward.";
+            }
+        }
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>() {};
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "ad" };
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if (player == null)
+            {
+                Rocket.Core.Logging.Logger.Log("This command cannot be called from the console.");
+                return;
+            }
+
+            if (!Main.OnCooldown(player) && Main.Connected == true)
+            {
+                Main.request_link(player);
+            }
+            else if (Main.OnCooldown(player))
+            {
+                Dictionary<string, Color> translation = Main.getTranslation("COOLDOWN");
+                foreach (var translation_pair in translation)
+                {
+                    UnturnedChat.Say((IRocketPlayer)player, translation_pair.Key, translation_pair.Value);
+                }
+            }
+            else if (Main.Connected == false)
+            {
+                UnturnedChat.Say(player, "There was error while connecting to HUB. Try again later.");
+            }
+            else
+            {
+                UnturnedChat.Say(player, "Error while processing your request.");
+            }
+        }
+    }
+
+    class CommandCooldown : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Player; }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "cooldown";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Tells player how much time is left before cooldown expiry.";
+            }
+        }
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>() {};
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "cooldown" };
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if (player == null)
+            {
+                Rocket.Core.Logging.Logger.Log("This command cannot be called from the console.");
+                return;
+            }
+
+            var data = Main.CooldownTime(player);
+            if (data != "")
+            {
+                Dictionary<string, Color> translation = Main.getTranslation("COOLDOWN_REMAINING", data);
+                foreach (var translation_pair in translation)
+                {
+                    UnturnedChat.Say((IRocketPlayer)player, translation_pair.Key, translation_pair.Value);
+                }
+            }
+            else
+            {
+                Dictionary<string, Color> translation = Main.getTranslation("NOT_ON_COOLDOWN");
+                foreach (var translation_pair in translation)
+                {
+                    UnturnedChat.Say((IRocketPlayer)player, translation_pair.Key, translation_pair.Value);
+                }
+            }
+        }
+    }
+
+    class CommandClearCooldownAll : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Both; }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "clearallcooldown";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Clears all cooldowns on the server.";
+            }
+        }
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>() { };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "clearall" };
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            Main.Cooldown.Clear();
+            Main.Ad_Views.Clear();
+            Rocket.Core.Logging.Logger.Log("Cooldown list cleared.");
+        }
+    }
+
+    //Fix 
+    class CommandClearCooldownPlayer : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Both; }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "clearcooldown";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Clears cooldown for player.";
+            }
+        }
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>() { };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "<player>";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "clear" };
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if (command.Length == 1)
+            {
+                UnturnedPlayer remPlayer = UnturnedPlayer.FromName(command[0]);
+                if (remPlayer != null)
+                {
+                    Main.Cooldown.Remove(remPlayer.CSteamID);
+                    Main.Ad_Views.Remove(remPlayer.CSteamID);
+                    Rocket.Core.Logging.Logger.Log("Cleared cooldown for player " + remPlayer.DisplayName);
+                }
+                else
+                {
+                    Rocket.Core.Logging.Logger.LogWarning("Tried to clear cooldown for " + command[0] + " but he is not on the server!");
+                }
+            }
+            else
+            {
+                if(caller == null)
+                {
+                    Rocket.Core.Logging.Logger.Log("Wrong syntax of command");
+                }
+                else
+                {
+                    UnturnedChat.Say(player, "Wrong syntax of command");
+                }
+            }
+        }
+    }
+}
---
+++ MOTDgd/Configuration.cs
@@ -0,0 +1,113 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MOTDgd
+{
+    public class MOTDgdConfiguration : IRocketPluginConfiguration
+    {
+        public int User_ID; 
+        public int Reminder_delay; 
+        public int Number_of_ads_before_cooldown; 
+        public string Reward_mode; 
+        public int CooldownTime; 
+        public bool Global_messages; 
+        public bool Ad_on_join; 
+        public bool Reapply_join_command;
+        public bool Give_reward_when_video_unavailable;
+        public bool AdvancedLogging; 
+        public string Executor_CSteamID;
+        public List<string> Join_Commands;
+
+        public sealed class Reward
+        {
+            [XmlAttribute("Command")]
+            public string Command;
+
+            [XmlAttribute("Probability")]
+            public int Probability;
+
+            public Reward(string command, int probability)
+            {
+                Command = command;
+                Probability = probability;
+            }
+
+            public Reward()
+            {
+                Command = "";
+                Probability = 1;
+            }
+        }
+
+        public sealed class Translation
+        {
+            [XmlAttribute("Identifier")]
+            public string Identifier;
+
+            [XmlAttribute("Text")]
+            public string Text;
+
+            [XmlAttribute("Color")]
+            public string Color;
+
+            public Translation(string identifier, string text, string color)
+            {
+                Identifier = identifier;
+                Text = text;
+                Color = color;
+            }
+
+            public Translation()
+            {
+                Identifier = "";
+                Text = "";
+                Color = "";
+            }
+        }
+        [XmlArrayItem("Reward")]
+        [XmlArray(ElementName = "Rewards")]
+        public Reward[] Rewards;
+
+        [XmlArrayItem("Translation")]
+        [XmlArray(ElementName = "Translations")]
+        public Translation[] Translations;
+
+        public void LoadDefaults()
+        {
+            User_ID = 0;
+            Rewards = new Reward[]{
+                new Reward("Reward",1)
+            };
+            Join_Commands = new List<string>() { "broadcast (player) connected to the server!" };
+            Reminder_delay = 5; 
+            Number_of_ads_before_cooldown = 1;
+            Reward_mode = "ALL";
+            CooldownTime = 15;
+            Global_messages = true;
+            Ad_on_join = false;
+            Give_reward_when_video_unavailable = false;
+            Executor_CSteamID = "";
+            AdvancedLogging = false;
+
+            Translations = new Translation[]{
+                new Translation("EVENT_RECEIVED_REWARD_COOLDOWN", "You got your reward! Now you are on cooldown for {0} minutes", "default"),
+                new Translation("EVENT_RECEIVED_REWARD_ADS_REMAIN", "You got your reward! You can watch {0} more ads before receiving cooldown.", "default"),
+                new Translation("LINK_RESPONSE", "Here's your link!", "!not_required!"),
+                new Translation("EVENT_RECEIVED_REWARD_ADS_GLOBAL", "{0} received his reward by watching ad. Get your reward with /ad command", "default"),
+                new Translation("REMINDER_MESSAGE", "Get your reward by using /ad command!", "default"),
+                new Translation("REMINDER_MESSAGE_JOIN", "oin commands have been reapplied!. Get your reward by using /ad command!", "default"),
+                new Translation("COOLDOWN_EXPIRED", "Your cooldown has just expired!", "default"),
+                new Translation("COOLDOWN", "You already received reward and now are on cooldown!", "default"),
+                new Translation("COOLDOWN_REMAINING", "You can receive a new reward in {0} minutes!", "default"),
+                new Translation("NOT_ON_COOLDOWN", "You can watch ad right now!", "default"),
+                new Translation("REQUEST_LINK_MESSAGE", "Requesting your ad.", "default"),
+                new Translation("COMPLETED_WITHOUT_VIDEO", "Unfortunately we didn't have any video for you, so you can't receive your reward.", "default")
+            };
+        }
+
+    }
+}
